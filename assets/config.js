9fbfc49d0ea04aeb8bab04e35677296f82c84f93
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-18 15:45:02 +0200

    release: expose runtime version 1.2.0

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.PGI_CONFIG = Object.freeze({
   appName: "PGI Telecom • Audiotel Premium Pro",
-  version: "1.1.0",
+  version: "1.2.0",
   schemaVersion: 1,
   mode: "demo",
   apiBaseUrl: "",
