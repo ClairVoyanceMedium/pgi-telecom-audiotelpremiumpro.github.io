bd7615c0a7a9afb2b2d49d5a66d3f990477387b2
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-18 22:15:30 +0200

    brand: rename runtime app

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.PGI_CONFIG = Object.freeze({
-  appName: "PGI • Telecom Audiotel Premium Pro",
-  version: "1.10.0",
+  appName: "PGI • Telecom - Audiotel Premium Pro",
+  version: "1.10.1",
   releaseId: "demo",
   schemaVersion: 1,
   mode: "demo",
