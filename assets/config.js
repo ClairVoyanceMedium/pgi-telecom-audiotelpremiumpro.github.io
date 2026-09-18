d3ec7fe91584f364f5256e357a91a1c21ac3171f
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-19 01:11:10 +0200

    release: bump demo runtime to 1.16.0

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.PGI_CONFIG = Object.freeze({
   appName: "PGI • Telecom - Audiotel Premium Pro",
-  version: "1.15.0",
+  version: "1.16.0",
   releaseId: "demo",
   schemaVersion: 1,
   mode: "demo",
