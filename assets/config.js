8eaa2765bce99621b62e503dbeba4c6eb496604d
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-18 16:26:54 +0200

    release: expose dashboard version 1.3.0

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.PGI_CONFIG = Object.freeze({
   appName: "PGI Telecom • Audiotel Premium Pro",
-  version: "1.2.0",
+  version: "1.3.0",
   schemaVersion: 1,
   mode: "demo",
   apiBaseUrl: "",
