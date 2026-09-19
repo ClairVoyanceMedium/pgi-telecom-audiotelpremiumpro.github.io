832ae8922b4d54da13839d8f96d23e070f76e76c
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-19 22:46:30 +0200

    release: align assets/config.js to 1.23.0

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.PGI_CONFIG = Object.freeze({
   appName: "PGI • Telecom - Audiotel Premium Pro",
-  version: "1.22.0",
+  version: "1.23.0",
   releaseId: "demo",
   schemaVersion: 1,
   mode: "demo",
