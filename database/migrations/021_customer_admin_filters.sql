7a1dd3c13d5be2a14372804965c0f2306f03927d
ClairVoyanceMedium <contact.clairvoyancemedium@gmail.com>
2026-09-19 22:43:31 +0200

    db: avoid redundant customer directory index

--- database/migrations/021_customer_admin_filters.sql
+++ database/migrations/021_customer_admin_filters.sql
@@ -4,8 +4,4 @@ BEGIN;
 CREATE INDEX IF NOT EXISTS tenant_kyc_status_tenant_idx
   ON tenant_kyc_profiles(status,tenant_id);
 
-CREATE INDEX IF NOT EXISTS tenants_admin_status_country_id_idx
-  ON tenants(status,country_code,id DESC)
-  WHERE tenant_type <> 'internal';
-
 COMMIT;
